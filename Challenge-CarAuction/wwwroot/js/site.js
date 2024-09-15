ca34646801b8c98c3f6a32e786edbbdfbf8c09f4
Rui Dias <ruippdias99@gmail.com>
2024-09-16 00:50:38 +0100

    Correct filtering and viewport

--- Challenge-CarAuction/wwwroot/js/site.js
+++ Challenge-CarAuction/wwwroot/js/site.js
@@ -1,8 +1,20 @@
 ﻿$(document).ready(function () {
-    $('table').DataTable({
-        "paging": true,
-        "searching": true,
-        "ordering": true,
-        "info": true
+    $('table thead th').each(function (i) {
+        var title = $(this).text();
+        $(this).html(
+            '<input type="text" placeholder="' + title + '" data-index="' + i + '" />'
+        );
+    });
+
+    // Initialize DataTables with FixedColumns
+    var table = $('table').DataTable({
+        paging: false,
+    });
+
+    // Add search functionality to header inputs
+    $('table thead input').on('keyup', function () {
+        var index = $(this).data('index');
+        var value = $(this).val();
+        table.column(index).search(value).draw();
     });
 });
